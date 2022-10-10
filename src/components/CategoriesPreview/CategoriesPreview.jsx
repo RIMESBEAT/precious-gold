07e94e707caeeb26adbe4807ffb022ea36f98d7e
RIMESBEAT <100046980+RIMESBEAT@users.noreply.github.com>
2022-10-10 10:41:16 +0100

    padding added to category css

--- src/components/CategoriesPreview/CategoriesPreview.jsx
+++ src/components/CategoriesPreview/CategoriesPreview.jsx
@@ -5,16 +5,16 @@ import CategoryPreview from "../CategoryPreview/CategoryPreview";
 function CategoriesPreview() {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <>
+    <div className="category__container__box">
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
         return (
-          <div className="grid__box" key={title}>
+          <div className="grid__box " key={title}>
             <CategoryPreview key={title} title={title} products={products} />
           </div>
         );
       })}
-    </>
+    </div>
   );
 }
 
